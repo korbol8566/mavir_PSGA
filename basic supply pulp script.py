02f44a17ee01ef35cef24535e8f93b28926be366
Boldizsar <boldizsar.kormendy@gmail.com>
2022-11-21 08:56:11 +0100

    Added negavtive scenario

--- basic supply pulp script.py
+++ basic supply pulp script.py
@@ -1,69 +1,34 @@
-# basic supply simplex script based on the supply xls
-from PIL.EpsImagePlugin import binary
-from pulp import *
-
-# # Create the 'prob' variable to contain the problem data
-# prob = LpProblem("The Basic Supply Problem", LpMinimize)
+# from pulp import *
 #
-# # Variables are added (with a lower limit of 0):
-# # variable_name = LpVariable("arbitrary name", lower bound, upper bound,datatype)
-# vendor1 = LpVariable("vendor1", 0, 12, LpInteger)
+# problemType = LpMaximize
+# prob = LpProblem("The Basic Supply Problem", problemType)
+#
+# vendor1 = LpVariable("vendor1", 0, 10, LpInteger)
 # vendor2 = LpVariable("vendor2", 0, 34, LpInteger)
 # vendor3 = LpVariable("vendor3", 0, 24, LpInteger)
 #
-# # The objective function is added to 'prob' first (cost * vendor)
-# prob += 95 * vendor1 + 110 * vendor2 + 120 * vendor3, "Current Optimal Supply Cost"
+# prob += -100 * vendor1 + -110 * vendor2 + -120 * vendor3, "Current Optimal Supply Cost"
+# prob += vendor1 + vendor2 + vendor3 == 54, "Demand"
 #
-# # The  constraints are entered (supply must equal demand = 50)
-# prob += vendor1 + vendor2 + vendor3 == 50, "Demand"
-#
-# # The problem is solved using PuLP's choice of Solver
 # prob.solve()
 #
-# # The status of the solution is printed to the screen
 # print("Status:", LpStatus[prob.status])
 #
-# # Each of the variables is printed with it's resolved optimum value
 # for v in prob.variables():
 #     print(v.name, "=", v.varValue)
 #
-# # The optimised objective function value is printed to the screen
 # print("Current Optimal Supply Cost = ", value(prob.objective))
 
-# todo: user input demand
-# todo:
-# todo:
-# todo:
-
-# Create the 'prob' variable to contain the problem data
-prob = LpProblem("The Basic Supply Problem", LpMinimize)
-
-# Variables are added (with a lower limit of 0):
-# variable_name = LpVariable("arbitrary name", lower bound, upper bound,datatype)
-vendor1 = LpVariable("vendor1", 0, 10, LpInteger)
-vendor2 = LpVariable("vendor2", 0, 34, LpInteger)
-vendor3 = LpVariable("vendor3", 0, 24, LpInteger)
-binaryVendor1 = LpVariable("vendor4", 10, 10, LpBinary)
-
-# The objective function is added to 'prob' first (cost * vendor)
-prob += 100 * vendor1 + 110 * vendor2 + 120 * vendor3 + 10 * binaryVendor1, "Current Optimal Supply Cost"
-
-# The  constraints are entered (supply must equal demand = 50)
-prob += vendor1 + vendor2 + vendor3 + 10 * binaryVendor1 == 10, "Demand"
+print("Start")
+mainCounter = 0
+subCounter = 0
+while mainCounter < 10:
+    print("Main Counter = ", mainCounter)
+    while subCounter < 4:
+        print("Sub Counter = ", subCounter)
+        subCounter += 1
+    subCounter = 0
+    mainCounter += 1
 
 
-# The problem is solved using PuLP's choice of Solver
-prob.solve()
-
-# The status of the solution is printed to the screen
-print("Status:", LpStatus[prob.status])
-
-# Each of the variables is printed with it's resolved optimum value
-for v in prob.variables():
-    if v.name == binaryVendor1.name:
-        print(v.name, "=", 10 * v.varValue)
-    else:
-        print(v.name, "=", v.varValue)
-
-# The optimised objective function value is printed to the screen
-print("Current Optimal Supply Cost = ", value(prob.objective))
+print("End")
--- mavir mvp.py
+++ mavir mvp.py
@@ -4,11 +4,10 @@
 # "POSITIVE SUPPLY" SCENARIO
 # todo: timestamped xls output
 
-import pandas as pd # Dataframes
-from pulp import * # Simplex
-import uuid # Unique ID in output file
-import json # pretty printing output dict
-import csv # main output
+import pandas as pd  # Dataframes
+from pulp import *  # Simplex
+import uuid  # Unique ID in output file
+import json  # pretty printing output dict
 
 # xls data is read into pandas dataframes and sorted into list, which are then fed into the algorithm!
 # a pandas dataframe is used to store xls data
@@ -18,19 +17,48 @@ df = pd.read_excel('supply.xlsx', sheet_name='00')
 
 # target demand is read into a pandas dataframe
 
+supplyProductStartHour = 0
+supplyProductEndHour = 1
+supplyProductTime = "0{}:00-0{}:00.".format(supplyProductStartHour, supplyProductEndHour)
+
+if supplyProductStartHour < 10 & supplyProductEndHour < 10:
+    supplyProductTime = "0{}:00-0{}:00.".format(supplyProductStartHour, supplyProductEndHour)
+
+if supplyProductStartHour < 10 & supplyProductEndHour >= 10:
+    supplyProductTime = "0{}:00-{}:00.".format(supplyProductStartHour, supplyProductEndHour)
+
+if supplyProductStartHour >= 10 & supplyProductEndHour >= 10:
+    supplyProductTime = "{}:00-{}:00.".format(supplyProductStartHour, supplyProductEndHour)
+
 targetDemand_with_nan = demand_df['SZUMMA'].tolist()
-targetDemand = [x for x in targetDemand_with_nan if str(x) != 'nan']
+targetDemandList = [x for x in targetDemand_with_nan if str(x) != 'nan']
+targetDemand = targetDemandList[0]
+if targetDemand > 0:
+    problemType = LpMinimize
+    supplyDirection = "Negativ / Negative"
+else:
+    problemType = LpMaximize
+    supplyDirection = "Pozitiv / Positive"
 
 # vendor names are parsed out from filtered df's while handling autofilled nan values
-afrr_df = df[df['Piac / Market'] == 'aFRR / aFRR']
+afrr_df = \
+    df[(df['Piac / Market'] == 'aFRR / aFRR')
+       & (df['Irany / Direction'] == supplyDirection)
+       & (df['Termek / Product'] == supplyProductTime)]
+
 afrr_ids = afrr_df.index.astype(str).tolist()
-afrr_df.insert(loc=0, column='row_id', value = afrr_ids)
+afrr_df.insert(loc=0, column='row_id', value=afrr_ids)
 afrr_df['Piac / Market'] = afrr_df['Piac / Market'] + afrr_df['row_id']
 afrrVendors = afrr_df['Piac / Market'].tolist()
 
-mfrr_df = df[df['Piac / Market'] == 'mFRR es RR / mFRR and RR']
+
+mfrr_df = \
+    df[(df['Piac / Market'] == 'mFRR es RR / mFRR and RR')
+       & (df['Irany / Direction'] == supplyDirection)
+       & (df['Termek / Product'] == supplyProductTime)]
+
 mfrr_ids = mfrr_df.index.astype(str).tolist()
-mfrr_df.insert(loc=0, column='row_id', value = mfrr_ids)
+mfrr_df.insert(loc=0, column='row_id', value=mfrr_ids)
 mfrr_df['Piac / Market'] = mfrr_df['Piac / Market'] + mfrr_df['row_id']
 mfrrVendors = mfrr_df['Piac / Market'].tolist()
 
@@ -52,7 +80,7 @@ afrrVendorVolumes = dict(zip(afrrVendors, afrrVendorVolumeList))
 mfrrVendorVolumes = dict(zip(mfrrVendors, mfrrVendorVolumeList))
 
 # Create the 'prob' variable to contain the problem data
-prob = LpProblem("The MAVIR MVP Optimization Problem", LpMinimize)
+prob = LpProblem("The MAVIR MVP Optimization Problem", problemType)
 
 # A dict called 'vendor_vars' is created to contain the referenced Variables, with lower bounds of 0
 afrrVendorVars = LpVariable.dicts("afrrVendor", afrrVendors, 0)
@@ -65,7 +93,7 @@ prob += (
     "Optimal Cost to Fill Supply",
 )
 # The  constraints are added to 'prob'
-prob += (lpSum(afrrVendorVars[i] for i in afrrVendors) + lpSum(mfrrVendorVars[i] for i in mfrrVendors)) == targetDemand[0], "Demand"
+prob += (lpSum(afrrVendorVars[i] for i in afrrVendors) + lpSum(mfrrVendorVars[i] for i in mfrrVendors)) <= targetDemand, "Demand"  # alá lövünk a dolognak, optimumban =, nem optimumban <
 
 for i in afrrVendors:
     prob += afrrVendorVars[i] <= afrrVendorVolumes[i]
@@ -97,7 +125,7 @@ for v in prob.variables():
             print(v.name, "=", v.varValue)
             output_dict["Unique ID"].append(str(uuid.uuid4()))
             output_dict["Időpont"].append(demand_df.at[0, 'Időpont'])  # todo: iterandus legyen
-            output_dict["Kereslet (MW)"].append(targetDemand[0])
+            output_dict["Kereslet (MW)"].append(targetDemand)
             output_dict["Kínálat (HUF)"].append(value(prob.objective))
             output_dict["Optimális eredmény?"].append(LpStatus[prob.status])
             output_dict["Piac"].append(v.name)
@@ -114,5 +142,3 @@ output_df = pd.DataFrame(output_dict).to_excel('MAVIR_PSGA_simplex_output_{}.xls
 
 # The optimised objective function value is printed to the screen
 # print("Current Optimal Supply Cost = ", value(prob.objective))
-
-
